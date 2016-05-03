3cf774fd66b2817bda6025e62cbc0fc67a630a44
Aditya Kashi <aditya.kashi@yahoo.com>
2016-05-03 15:39:55 -0400

    exp

---
+++ build/utilities/CMakeFiles/convertformat.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/convertformat.dir/convertformat.cpp.o"
+  "../bin/convertformat.pdb"
+  "../bin/convertformat"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/convertformat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
