7fb8b62bcb492717e62580183042feb2b15fafa4
Aditya Kashi <aditya.kashi@mail.mcgill.ca>
2017-06-14 18:11:55 -0400

    edited scaling plot to display ideal line and strong efficiency

--- testcases/plotscale.py
+++ testcases/plotscale.py
@@ -14,17 +14,27 @@ data = np.genfromtxt(fname)
 n = data.shape[0]
 data[:,1:] = 1.0/data[:,1:]
 
+refline = np.zeros(n)
+refline[:] = data[:,0]
+refline = refline/data[0,0]*data[0,1]
+
 psigy = data[:,1].sum()
 sigx = data[:,0].sum()
 sigx2 = (data[:,0]*data[:,0]).sum()
 psigxy = (data[:,1]*data[:,0]).sum()
 
-pslope = (n*psigxy-sigx*psigy)/(n*sigx2-sigx**2)
-print("Slope is " + str(pslope))
+psigref = refline.sum()
+psigxref = (refline*data[:,0]).sum()
 
-plt.plot(data[:,0],data[:,1],'bo-', label="Wall time")
-#plt.plot(data[:,0],data[:,2],'gs-', label="Wall time")
-plt.title("Strong scaling")
+pslope = (n*psigxy-sigx*psigy)/(n*sigx2-sigx**2)
+refslope = (n*psigxref-sigx*psigref)/(n*sigx2-sigx**2)
+print("Slope is " + str(pslope) + ", ideal slope is " + str(refslope))
+print("Strong scaling ratio = {}".format(pslope/refslope))
+
+plt.plot(data[:,0],data[:,1],'bo-', label="Speed w.r.t Walltime")
+plt.plot(data[:,0],refline,'gs--', label="Ideal")
+#plt.plot(data[:,0],data[:,2],'gs-', label="CPU time")
+plt.title("Strong scaling {:.3}".format(pslope/refslope))
 plt.xlabel("Number of threads")
 plt.ylabel("1.0 / Time")
 plt.legend()
