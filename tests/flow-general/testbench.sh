ec7401fb29efb077fbafb18c250dda5feb623c17
Aditya Kashi <aditya.kashi@mail.mcgill.ca>
2018-11-04 19:35:45 -0500

    fixed threaded perftest test with slurm

--- tests/flow-general/testbench.sh
+++ tests/flow-general/testbench.sh
@@ -2,14 +2,8 @@
 
 # Execute an async threads-speedup benchmark and check the report generated
 
-@CMAKE_BINARY_DIR@/bench_threads_async $@
+@SEQEXEC@ @THREADOPTS@ @SEQTASKS@ @CMAKE_BINARY_DIR@/bench_threads_async $@
 
 @CMAKE_CURRENT_BINARY_DIR@/check_bench_output @CMAKE_CURRENT_BINARY_DIR@/2dcyl
-retval=$?
 
-if [ $retval -eq 0 ]
-then
-	return 0
-else
-	return 2
-fi
+exit
