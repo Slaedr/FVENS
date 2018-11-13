f86ac8cc056abc311671043db978727d911ec5de
Aditya <adityakashi65@gmail.com>
2018-11-13 01:56:17 -0500

    upgraded strong scaling plotter and improved conv plotter

--- scripts/plotconv.py
+++ scripts/plotconv.py
@@ -19,7 +19,7 @@ def setAxisParams(ax, baseLineWidth):
     ax.grid(which='major', axis='y', lw=0.5*baseLineWidth, linestyle='-', color='0.75')
     ax.grid(which='minor', axis='y', lw=0.5*baseLineWidth, linestyle=':', color='0.75')
 
-def plotquantity(filelist, quantname, numits, labellist, opts):
+def plotquantity(filelist, quantname, numits, labellist, opts, imageformatstring):
     plt.close()
     markdivisor = 20
     for i in range(len(filelist)):
@@ -38,7 +38,8 @@ def plotquantity(filelist, quantname, numits, labellist, opts):
                     mew=opts['markedgewidth'], \
                     markevery=list(range(0,pltdatalen,opts['markinterval'])), \
                     label=labellist[i])
-            plt.xlabel("Pseudo-time steps")
+            plt.xlabel("Pseudo-time steps", fontsize="medium")
+            plt.ylabel("Log of L2 norm of energy residual", fontsize="medium")
         elif quantname == "walltime":
             plt.plot(data[numits:,3], data[numits:,1], \
                     lw=opts['linewidth'], ls=opts['linetype'][i], color=opts['colorlist'][i], \
@@ -46,14 +47,23 @@ def plotquantity(filelist, quantname, numits, labellist, opts):
                     mew=opts['markedgewidth'], \
                     markevery=list(range(0,pltdatalen,opts['markinterval'])), \
                     label=labellist[i])
-            plt.xlabel("Wall-clock time (s)")
+            plt.xlabel("Wall-clock time (s)", fontsize="medium")
+            plt.ylabel("Log of L2 norm of energy residual", fontsize="medium")
+        elif quantname == "cfl":
+            plt.plot(data[numits:,0], data[numits:,6], \
+                    lw=opts['linewidth'], ls=opts['linetype'][i], color=opts['colorlist'][i], \
+                    marker=opts['marklist'][i], ms=opts['marksize'], \
+                    mew=opts['markedgewidth'], \
+                    markevery=list(range(0,pltdatalen,opts['markinterval'])), \
+                    label=labellist[i])
+            plt.xlabel("Pseudo-time steps", fontsize="medium")
+            plt.ylabel("CFL number", fontsize="medium")
 
-        plt.ylabel("Log of L2 norm of energy residual")
         ax = plt.axes()
         setAxisParams(ax,opts['linewidth'])
         plt.legend(loc="best", fontsize="medium")
 
-    plt.savefig(filename.split('/')[-1].split('.')[0]+"-"+quantname+".ps")
+    plt.savefig(filename.split('/')[-1].split('.')[0]+"-"+quantname+"." + imageformatstring, dpi=200)
 
 if __name__ == "__main__":
 
@@ -61,7 +71,7 @@ if __name__ == "__main__":
             "marklist" : ['.', 'x', '+', '^', 'v', '<', '>', 'd'],
             "colorlist" : ['k', 'b', 'r', 'g', 'c', 'm', 'k'],
             "linetype" : ['-', '--', '-.', ':', '--', '--'],
-            "linewidth" : 1,
+            "linewidth" : 0.75,
             "marksize" : 5,
             "markedgewidth" : 1 \
             }
@@ -73,9 +83,12 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Plots residual history w.r.t. iterations and wall time starting at a specified iteration")
     parser.add_argument("files", nargs='+')
     parser.add_argument("--labels", nargs='+', help = "Legend strings")
-    parser.add_argument("--start_iter", type=int, default=100, help = "Iteration to start plotting from")
+    parser.add_argument("--labelstr", default="", help = "Common suffix for legend strings")
+    parser.add_argument("--start_iter", type=int, default=0, help = "Iteration to start plotting from")
+    parser.add_argument("--format", default="png", help = "Output format")
     args = parser.parse_args(sys.argv)
 
-    plotquantity(args.files[1:], "residual", args.start_iter, args.labels, opts)
-    plotquantity(args.files[1:], "walltime", args.start_iter, args.labels, opts)
+    plotquantity(args.files[1:], "residual", args.start_iter, args.labels, opts, args.format)
+    plotquantity(args.files[1:], "walltime", args.start_iter, args.labels, opts, args.format)
+    plotquantity(args.files[1:], "cfl", args.start_iter, args.labels, opts, args.format)
 
