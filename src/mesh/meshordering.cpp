7dbdb50902cbc40dde46f85f6432ca8cd7591b2a
Aditya Kashi <aditya.kashi@mail.mcgill.ca>
2019-02-17 19:41:35 -0500

    added weight computation function

--- src/mesh/meshordering.cpp
+++ src/mesh/meshordering.cpp
@@ -19,11 +19,13 @@
 
 #include <utility>
 #include "meshordering.hpp"
+#include "ameshutils.hpp"
 
 namespace fvens {
 
+/// Returns the edge weights about each cell, ordered by decreasing weight
 template <typename scalar> static
-std::pair<amat::Array2d<scalar>,amat::Array2d<a_int>> computeWeights(const UMesh2dh<scalar>& m);
+std::pair<amat::Array2d<a_real>,amat::Array2d<EIndex>> computeWeights(const UMesh2dh<scalar>& m);
 
 template <typename scalar>
 void lineReorder(UMesh2dh<scalar>& m, const double threshold)
@@ -31,12 +33,53 @@ void lineReorder(UMesh2dh<scalar>& m, const double threshold)
 }
 
 template <typename scalar>
-std::pair<amat::Array2d<scalar>,amat::Array2d<a_int>> computeWeights(const UMesh2dh<scalar>& m)
+std::pair<amat::Array2d<a_real>,amat::Array2d<EIndex>> computeWeights(const UMesh2dh<scalar>& m)
 {
-	amat::Array2d<scalar> weights(m.gnelem(),2);
-	amat::Array2d<a_int> faceNbrIdx(m.gnelem(),2);
+	amat::Array2d<a_real> aniso(m.gnelem(), m.gmaxnfael());
+	amat::Array2d<EIndex> faceIdx(m.gnelem(), m.gmaxnfael());
 
-	return std::make_pair(weights,faceNbrIdx);
+	amat::Array2d<scalar> ccentres(m.gnelem(),NDIM);
+#pragma omp parallel for default(shared)
+	for(a_int iel = 0; iel < m.gnelem(); iel++)
+	{
+		kernel_computeCellCentreAoS(m, iel, &ccentres(iel,0));
+	}
+
+#pragma omp parallel for default(shared)
+	for(a_int iel = 0; iel < m.gnelem(); iel++)
+	{
+		std::vector<std::pair<a_real,EIndex>> elaniso(m.gnfael(iel));
+		a_real avgw = 0;
+		for(EIndex j = 0; j < m.gnfael(iel); j++)
+		{
+			const a_int jel = m.gelemface(iel,j);
+			a_real dist = 0;
+			for(int idim = 0; idim < NDIM; idim++)
+				dist += std::pow(getvalue(ccentres(iel,idim)-ccentres(jel,idim)),2);
+
+			elaniso[j].first = 1.0/std::sqrt(dist);
+			avgw += elaniso[j].first;
+
+			elaniso[j].second = j;
+		}
+
+		avgw /= m.gnfael(iel);
+		for(EIndex j = 0; j < m.gnfael(iel); j++)
+			elaniso[j].first /= avgw;
+
+		// sort by *decreasing* weight
+		std::sort(elaniso.begin(), elaniso.end(),
+		          [](std::pair<a_real,EIndex> a, std::pair<a_real,EIndex> b) {
+			          return a.first > b.first;
+		          });
+
+		for(EIndex j = 0; j < m.gnfael(iel); j++) {
+			aniso(iel,j) = elaniso[j].first;
+			faceIdx(iel,j) = elaniso[j].second;
+		}
+	}
+
+	return std::make_pair(aniso,faceIdx);
 }
 
 template void lineReorder(UMesh2dh<a_real>& m, const double threshold);
